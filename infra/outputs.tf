1b45e1465297cff7af0fbd9a736b2c365196344e
AR4789 <amanr1871@gmail.com>
2025-09-24 16:03:11 +0530

    Frontend CI/CD Workflow

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,9 @@ output "cloud_run_url" {
 output "artifact_repo" {
   value = google_artifact_registry_repository.repo.repository_id
 }
+
+output "web_url" {
+  description = "The URL of the frontend Cloud Run service"
+  value       = google_cloud_run_service.web.status[0].url
+}
+
